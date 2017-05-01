2fb8972f6b1bc0ee3beb7ff87d7a03037407dba0
Parker.Kimbell <parker.kimbell@DS-iMAC27-16.local>
2017-05-01 10:11:41 -0700

    reposition amenity button text

--- public/app/maps.js
+++ public/app/maps.js
@@ -217,7 +217,7 @@ function floorHasThisLayer(floorPins, layer) {
 function buildLayerIcon(layer) {
   return $([
     "<li>",
-    "  <div class='category' data-categoryid=" + layer.Id + " style='background: url(" + layer.Icon + ") no-repeat; background-position: 50% 20%; background-size: 70%;'",
+    "  <div class='category' data-categoryid=" + layer.Id + " style='background: url(" + layer.Icon + ") no-repeat; background-position: 50% 20%; background-size: 70%;'>",
     "  <p>" + layer.Name,
     "  </p>",
     "  </div>",
