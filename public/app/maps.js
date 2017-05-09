0844f17623dedb0bfbe679c25249584341b9ede9
Parker.Kimbell <parker.kimbell@DS-iMAC27-16.local>
2017-05-08 17:11:57 -0700

    refactoring at a decent spot

--- public/app/maps.js
+++ public/app/maps.js
@@ -1,4 +1,4 @@
-var MapsApp = require('./init.js');
+var MapsApp = require('./MapsApp.js');
 // TODO: this will be the entry point for the app,
 // inject JavaScript with the CMS URL and go
 MapsApp.init("https://pwc.downstreamlabs.com");
